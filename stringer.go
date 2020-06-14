d095523b1a770e20dade0612d1d82cf593dce909
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-06-15 02:28:16 +0530

    Added v1.0.3

--- stringer.go
+++ stringer.go
@@ -3,6 +3,6 @@ package helper
 import "github.com/fatih/color"
 
 func Stringify(input string) (result string, err error) {
-	color.Cyan("1.0.2", input)
+	color.Cyan("v1.0.3", input)
 	return input + "*-", nil
 }
